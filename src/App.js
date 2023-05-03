44fede00522f09d5934f82bc4769befc184dabba
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-03 12:58:26 +0300

    Project Finished
    Simple To Do list created with React. You can add new item and it will be displayed on the screen with an option to be deleted afterwards.

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       </div>
       <div className='list'></div>
       {todoList.map((task) => {
-        return <div>
+        return <div className='task'>
           <h1>{task.taskName}</h1>
           <button onClick={() => deleteTask(task.id)}>Delete Task</button>
         </div>
